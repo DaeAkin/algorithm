c19d68ba992827eb606560230b5e091484d3296e
민동현 <mindonghyeon890@gmail.com>
2019-06-08 14:25:11 +0900

    링크드리스트 테스트코드 합침

--- algorism/src/datastructure/MySinglyLinkedList.java
+++ algorism/src/datastructure/MySinglyLinkedList.java
@@ -1,5 +1,11 @@
 package datastructure;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
 
 public class MySinglyLinkedList<E> {
 	
@@ -69,7 +75,6 @@ public class MySinglyLinkedList<E> {
 	public E set(int index,E element ) {
 		//get() 호출하기
 		E old = get(index);
-		// get이랑 코드가 대부분 겹치넹.. 일단하자..
 		// 커서를 head에 맞춘다.
 		Node node = head;
 		// index까지 node를 움직인다.
@@ -124,5 +129,39 @@ public class MySinglyLinkedList<E> {
 		}
 		sb.append(" ]");
 		return sb.toString();
+	}
+	
+	
+
+
+		@Test
+		public void myDoublyLinkedList() {
+			List<Integer> list = new MyDoublyLinkedlist<>();
+			
+			list.add(5);
+			list.add(3);
+			list.add(1);
+			list.add(4);
+			list.add(9);
+			
+			System.out.println(list.toString());
+			
+			assertThat(list.size(), is(5));
+			assertThat(list.get(2), is(1));
+			assertThat(list.get(4), is(9));
+			assertThat(list.indexOf(0), is(-1));
+			assertThat(list.indexOf(4), is(3));
+			assertThat(list.remove(1), is(3));
+			assertThat(list.size(), is(4));
+			list.set(0, 10);
+			assertThat(list.get(0), is(10));
+			assertThat(list.remove(0), is(10));
+			assertThat(list.size(), is(3));
+			System.out.println("5랑 3이랑 제거됨.");
+			System.out.println(list.toString());
+			System.out.println("2번째 Node의 값 : " + list.get(1));
+			assertThat(list.remove(2), is(9));
+			
+
 	}
 }
