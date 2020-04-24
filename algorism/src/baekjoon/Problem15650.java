13880698beea6e6b7ba10b4d6cd6fb2598e4ac40
민동현 <mindonghyeon890@gmail.com>
2020-04-24 15:43:38 +0900

    백준 15650 완료

---
+++ algorism/src/baekjoon/Problem15650.java
@@ -0,0 +1,39 @@
+package baekjoon;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Problem15650 {
+
+    static int N;
+    static int M;
+    static Stack<Integer> alreadyIn;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        alreadyIn = new Stack<>();
+        recur("", 0,1);
+        System.out.println(sb.toString());
+    }
+
+    public static void recur(String str, int number,int in) {
+
+        if (number == M) {
+            sb.append(str.trim() + "\n");
+        } else {
+            number++;
+            for (int i = in; i <= N; i++) {
+                if (!alreadyIn.contains(i)) {
+                    alreadyIn.push(i);
+                    recur(str + " " + i, number,++in);
+                    alreadyIn.pop();
+                }
+            }
+        }
+
+    }
+}
