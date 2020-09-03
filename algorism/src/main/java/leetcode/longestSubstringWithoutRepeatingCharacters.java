0ca68cc2e86f20874f4a9ebb95e3eac8a7dfb734
민동현 <mindonghyeon890@gmail.com>
2020-09-03 20:26:26 +0900

    더 좋은 케이스 추가

--- algorism/src/main/java/leetcode/longestSubstringWithoutRepeatingCharacters.java
+++ algorism/src/main/java/leetcode/longestSubstringWithoutRepeatingCharacters.java
@@ -1,7 +1,9 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,7 +25,44 @@ public class longestSubstringWithoutRepeatingCharacters {
          assertThat(solution.lengthOfLongestSubstring("a")).isEqualTo(1);
      }
 
-// 내 정답
+     //더더 좋은 정답
+    //Sliding Window를 최적화 한 방법으로, 복잡도가 O(n) 나온다.
+     static class BetterBetterSolution {
+         public int lengthOfLongestSubstring(String s) {
+             int n = s.length(), ans = 0;
+             int[] index = new int[128]; // current index of character
+             // try to extend the range [i, j]
+             for (int j = 0, i = 0; j < n; j++) {
+                 i = Math.max(index[s.charAt(j)], i);
+                 ans = Math.max(ans, j - i + 1);
+                 index[s.charAt(j)] = j + 1;
+             }
+             return ans;
+         }
+     }
+
+     //더 좋은 정답
+    // Sliding Window 이라는데 시간복잡도가 O(2n) 밖에 안나온다.
+     static class BetterSolution {
+         public int lengthOfLongestSubstring(String s) {
+             int n = s.length();
+             Set<Character> set = new HashSet<>();
+             int ans = 0, i = 0, j = 0;
+             while (i < n && j < n) {
+                 // try to extend the range [i, j]
+                 if (!set.contains(s.charAt(j))){
+                     set.add(s.charAt(j++));
+                     ans = Math.max(ans, j - i);
+                 }
+                 else {
+                     set.remove(s.charAt(i++));
+                 }
+             }
+             return ans;
+         }
+     }
+
+// 내 정답 O(n^2)
       static class Solution {
           public int lengthOfLongestSubstring(String s) {
 
