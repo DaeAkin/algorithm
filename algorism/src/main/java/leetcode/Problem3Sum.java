9c5c4d97cb82272c48422ccaa44f970b3a39878d
Donghyeon Min <mindonghyeon890@gmail.com>
2020-09-17 08:18:58 +0900

    완료

--- algorism/src/main/java/leetcode/Problem3Sum.java
+++ algorism/src/main/java/leetcode/Problem3Sum.java
@@ -1,63 +1,40 @@
 package leetcode;
 
-import org.assertj.core.util.Lists;
-import org.assertj.core.util.Sets;
-
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.Assert.assertThat;
-
+/*
+Runtime: 1153 ms, faster than 5.00% of Java online submissions for 3Sum.
+Memory Usage: 167.5 MB, less than 5.01% of Java online submissions for 3Sum.
+ */
 public class Problem3Sum {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-//        assertThat(solution.threeSum(new int[]{-1, 0, 1, 2, -1, -4})).isEqualTo(Arrays.asList(Arrays.asList(-1, -1, 2), Arrays.asList(-1, 0, 1)));
+        assertThat(solution.threeSum(new int[]{-1, 0, 1, 2, -1, -4})).isEqualTo(Arrays.asList(Arrays.asList(-1, -1, 2), Arrays.asList(-1, 0, 1)));
         assertThat(solution.threeSum(new int[]{})).isEqualTo(Arrays.asList());
         assertThat(solution.threeSum(new int[]{0})).isEqualTo(Arrays.asList());
-        assertThat(solution.threeSum(new int[]{0,0,0})).isEqualTo(Arrays.asList(0,0,0));
+
     }
 
     static class Solution {
         public List<List<Integer>> threeSum(int[] nums) {
-            List<List<Integer>> result = new ArrayList<>(new HashSet<>());
-
-            for (int i = 0; i < nums.length; i++) {
-                int x = nums[i];
-
-                List<Integer> twoSumZeroWithX = findTwoSumZeroWithX(x, nums);
-                if(!twoSumZeroWithX.isEmpty())
-                    result.add(twoSumZeroWithX);
-
-            }
-            return result.stream().distinct()
-                    .collect(Collectors.toCollection(ArrayList::new));
-        }
-
-        static List<Integer> findTwoSumZeroWithX(int x, int[] nums) {
-            List<Integer> result = new ArrayList<Integer>();
-            for (int i = 0; i < nums.length; i++) {
-                int y = nums[i];
-                if (y == x)
-                    continue;
-
-                for (int j = i + 1; j < nums.length - 1; j++) {
-                    int z = nums[j];
-
-                    if(x + y + z == 0) {
-                        result.add(x);
-                        result.add(y);
-                        result.add(z);
-                        Collections.sort(result);
-                        return result;
-                    }
-
+            Set<List<Integer>> res = new HashSet<>();
+            if (nums.length == 0) return new ArrayList<>(res);
+            Arrays.sort(nums);
+            for (int i = 0; i < nums.length - 2; i++) {
+                int j = i + 1;
+                int k = nums.length - 1;
+                while (j < k) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (sum == 0) res.add(Arrays.asList(nums[i], nums[j++], nums[k--]));
+                    else if (sum > 0) k--;
+                    else if (sum < 0) j++;
                 }
 
             }
+            return new ArrayList<>(res);
 
-            return result;
         }
     }
 }
